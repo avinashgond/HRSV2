6d6710b59d8fb9c6da022646281e83d218751679
avinashgond <avinashgond1598@gmail.com>
2025-08-01 23:08:27 +0530

    Cross Origin added

--- src/main/java/com/hotel/shivaji/web/ShivajiHotelController.java
+++ src/main/java/com/hotel/shivaji/web/ShivajiHotelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.hotel.shivaji.entities.Rooms;
 import com.hotel.shivaji.services.RoomService;
 
+@CrossOrigin(origins = "https://hrs-ui1.onrender.com")
 @RestController
 @RequestMapping("/shivaji-hotel")
 public class ShivajiHotelController {
